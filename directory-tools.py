0f4e473e7a00d856555453f83ae3e404a7e00006
Wendy Park <piaomeixian119@hotmail.com>
2021-06-06 14:12:03 +0900

    Create directory-tools.py

---
+++ directory-tools.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from datetime import datetime
+
+# create directory
+def create_dir(dir_name):
+    try:
+        os.makedirs(dir_name, exist_ok=True)
+    except OSError as error:
+        print("Directory '%s' can not be created")    
+
+# delete directory
+def delete_empty_dir(empty_dir):
+    try:
+        shutil.rmtree(empty_dir)
+    except Exception:
+        print("remove empty dir error")
+
+# sort directorires by time        
+def get_oldest_dir(dir_path):
+    dir_path = dir_path + "/*"
+    dirs =sorted(glob.glob(dir_path), key=os.path.getctime)
+        
+        
+# check disk available space
+def get_available_space_percent():
+    total, used, free = shutil.disk_usage("/")
+    free_percent = free/total
+    # print("Total: %d GiB" % (total // (2**30)))
+    # print("Used: %d GiB" % (used // (2**30)))
+    # print("Free: %d GiB" % (free // (2**30)))
+    return free_percent
+
+  
+# get current time
+def get_current_time():
+    now = datetime.now()
+    # year = now.strftime("%Y")
+    # month = now.strftime("%m")
+    # day = now.strftime("%d")
+    # time = now.strftime(('%H-%M-%S-%f')[:-3])
+    time = datetime.now().strftime('%H-%M-%S-%f%z')
+    # time = datetime.utcnow().strftime('%H-%M-%S-%f')[:-3]
+    return time
