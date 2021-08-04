4899bbf4d19c5257dbce3eda83625203f86a24dc
Wendy Park <piaomeixian119@hotmail.com>
2021-08-04 14:22:28 +0900

    Update check-memory-addr.py

--- check-memory-addr.py
+++ check-memory-addr.py
@@ -1,2 +1,10 @@
 x = 4
 print hex(id(x))
+
+### bytes[] to short[]
+temp = len(byte_arr)//2
+short_arr = struct.unpack('>'+'h'*temp, byte_arr)
+# option H: unsigned short
+# option h: short
+# option <: little endian
+# option >: big  endian
