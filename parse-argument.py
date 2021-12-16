8b02a82b55d868ee91bdc3452da53b708878e1bf
Wendy Park <piaomeixian119@hotmail.com>
2021-12-16 11:37:10 +0900

    Update parse-argument.py

--- parse-argument.py
+++ parse-argument.py
@@ -7,6 +7,23 @@ def main():
 
     args = parser.parse_args()
     X = args.X
+    
+    parser.add_argument('--streaming',
+                        action="store_true",
+                        required=False,
+                        default=False,
+                        help='Use streaming inference API. ' +
+                        'The flag is only available with gRPC protocol.')
+    parser.add_argument('-m',
+                        '--model-name',
+                        type=str,
+                        required=True,
+                        help='Name of model')   
+    
+    #FLAGS = parser.parse_args()
+    #FLAGS.model_name
+    # FLAGS.streaming
+    
 
 if __name__=="__main__":
     main()
