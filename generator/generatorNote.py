0cb356af82d097ae0c2fd0585d0ebbe0d084053b
Wendy Park <piaomeixian119@hotmail.com>
2021-12-28 14:01:56 +0900

    Create generatorNote.py

---
+++ generator/generatorNote.py
@@ -0,0 +1,35 @@
+#-------------------------------------
+#    Python Generators with a Loop
+#
+# example output:  "o l l e h"
+#-------------------------------------
+def rev_str(my_str):
+    length = len(my_str)
+    for i in range(length - 1, -1, -1):
+        yield my_str[i]
+
+
+# For loop to reverse the string
+for char in rev_str("hello"):
+    print(char)
+
+    
+#-----------------------------------------------------
+#     Generator Expression
+# 
+# ps: Simple generators can be easily created 
+# using generator expressions. 
+#-----------------------------------------------------
+
+# Initialize the list
+my_list = [1, 3, 6, 10]
+
+# square each term using list comprehension
+list_ = [x**2 for x in my_list]
+
+# same thing can be done using a generator expression
+# generator expressions are surrounded by parenthesis ()
+generator = (x**2 for x in my_list)
+
+print(list_)
+print(generator)
