188bf38b2cf3e9ad93b5d4b023047efd1ef006eb
Wendy Park <piaomeixian119@hotmail.com>
2021-12-28 14:11:09 +0900

    Update generatorNote.py

--- generator/generatorNote.py
+++ generator/generatorNote.py
@@ -33,3 +33,19 @@ generator = (x**2 for x in my_list)
 
 print(list_)
 print(generator)
+
+#-----------------------------------------------------
+#    Pipelining Generators
+#-----------------------------------------------------
+def fibonacci_numbers(nums):
+    x, y = 0, 1
+    for _ in range(nums):
+        x, y = y, x+y
+        yield x
+
+def square(nums):
+    for num in nums:
+        yield num**2
+
+print(sum(square(fibonacci_numbers(10))))
+ 
